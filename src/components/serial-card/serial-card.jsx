0e03b73222a484040f1f447807a34824ca50404b
burjepaul <burjepaul97@gmail.com>
2022-08-17 15:21:31 +0300

    After tvShows component

--- src/components/serial-card/serial-card.jsx
+++ src/components/serial-card/serial-card.jsx
@@ -3,13 +3,22 @@ import DateForm from "../date-form/dateForm.component";
 import { ReactComponent as StarLogo } from "../../assets/star.svg";
 
 export default function SerialCard({ movie }) {
+  function setCardImageWidth(displayWidth) {
+    if (displayWidth > 1500) return 3;
+    else if (displayWidth > 1300 && displayWidth <= 1500) return 3;
+    else if (displayWidth > 1000 && displayWidth <= 1300) return 3;
+    else if (displayWidth <= 1000) return 2;
+  }
+
   const { name, first_air_date, vote_average, vote_count } = movie;
   return (
     <div className="card">
       <h4 className="movie-title">{name}</h4>
       <img
         className="movie-image"
-        src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
+        src={`https://image.tmdb.org/t/p/w${setCardImageWidth(
+            window.innerWidth
+          )}00${movie.poster_path}`}
         alt={movie.name}
       />
       <div className="card-footer">
