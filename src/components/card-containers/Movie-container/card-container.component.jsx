f5c809b508fe0cb32c0701a5a0dbdd0bac1e0bcd
burjepaul <burjepaul97@gmail.com>
2022-08-19 17:55:00 +0300

    Some refactoring

--- src/components/card-containers/Movie-container/card-container.component.jsx
+++ src/components/card-containers/Movie-container/card-container.component.jsx
@@ -20,7 +20,7 @@ const CardContainer = ({ searchField }) => {
       .then((response) => response.json())
       .then((result) =>
         setPopularMovies(
-          result.results.slice(0, 12).sort((a, b) => b.popularity - a.popularity)
+          result.results.slice(0, 6).sort((a, b) => b.popularity - a.popularity)
         )
       );
   }, [searchField]);
