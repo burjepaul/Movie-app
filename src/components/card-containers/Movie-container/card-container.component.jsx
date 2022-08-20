c7f13ce9033da2f90cc237bf7955b046f8db7d26
burjepaul <burjepaul97@gmail.com>
2022-08-20 15:00:40 +0300

    Some refactoring 2

--- src/components/card-containers/Movie-container/card-container.component.jsx
+++ src/components/card-containers/Movie-container/card-container.component.jsx
@@ -1,36 +1,52 @@
-import {React, useState, useEffect} from "react";
+import { React, useState, useEffect } from "react";
 
 import Card from "../../cards-components/Movie-card/card.component";
 
 import { API_KEY } from "../../../config";
+import icons from "../../../assets/icons.svg";
 
 import "./card-container.css";
 
+const renderSpinner = () => {
+  return `
+    <div class="spinner">
+    <svg>
+      <use href="${icons}#icon-loader"></use>
+    </svg>
+  </div>
+    `;
+};
 
 const CardContainer = ({ searchField }) => {
   const [popularMovies, setPopularMovies] = useState([]);
 
-  useEffect(() => {
-    fetch(
-      `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${searchField}`,
-      {
-        method: "GET",
-      }
-    )
-      .then((response) => response.json())
-      .then((result) =>
-        setPopularMovies(
-          result.results.slice(0, 6).sort((a, b) => b.popularity - a.popularity)
-        )
-      );
-  }, [searchField]);
+  if (!popularMovies) renderSpinner();
 
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetch(
+        `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${searchField}`,
+        {
+          method: "GET",
+        }
+      );
+      const json = await data.json();
+        console.log(json)
+      setPopularMovies(
+        json.results.slice(0, 6).sort((a, b) => b.popularity - a.popularity)
+      );
+    };
+
+    fetchData();
+
+    console.log(fetchData());
+  }, [searchField]);
 
   if (popularMovies.length !== 0) {
     return (
       <div className="card-container">
         {popularMovies.map((movie) => (
-          <Card movie={movie} key={movie.id}/>
+          <Card movie={movie} key={movie.id} />
         ))}
       </div>
     );
